cf454d92c99bc7a48f319a1a697f1cf7d2983925
Hussain-ai <huser.adraj@gmail.com>
2022-11-06 08:15:03 +0300

    Update 2

--- react-router-portfolio/src/App.js
+++ react-router-portfolio/src/App.js
@@ -2,19 +2,52 @@ import React, { Component } from 'react'
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import About from './components/About/About'
-import Account from './components/Account/Account'
 import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Profile from './components/Profile/Profile'
 import Signup from './components/Signup/Signup'
 import Skills from './components/Skills/Skills'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Auth from './components/Auth/Auth'
+import LoginForm from './components/LoginForm';
 
 function App() {
+//   const adminUser = {
+//     email: 'admin@admin.com',
+//     password: 'admin123'
+//   }
+
+//   const [user, setUser]= useState ({name: '', email: ''})
+//   const [error, setError]= useState('')
+
+// const Login = details => {
+//   console.log(details)
+// }
+
+// const Logout = ()=>{
+//   console.log('Logout')
+// }
+
   return (
-      //Java Logic here
-      <>
-      {/* //html logic here */}
+<>
+
+
+    {/* <div>
+    {(user.email !='') ? (
+  <div>
+  <h2>Welcome, <span>{user.name}</span></h2>
+  <button>Logout</button>
+  </div>
+    ) : (
+      <LoginForm Login={Login} error={error}/>
+    )
+    )}
+    </div> */}
+
+
+
+
         <Router>
           <nav>
             <Link to="/">Home</Link>&nbsp;
@@ -26,10 +59,9 @@ function App() {
           <nav className='nav-2'>
             <Link to="/Signup">Signup</Link>&nbsp;
             <Link to="/Login">Login</Link>&nbsp;
-            <Link to="/Account">Account</Link>&nbsp;
+            <Link to="/Auth">Contact</Link>&nbsp;
           </nav>
           
-          {/* <Home/> */}
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About/>}></Route>
@@ -38,7 +70,7 @@ function App() {
             <Route path="/skills" element={<Skills/>}></Route>
             <Route path="/signup" element={<Signup/>}></Route>
             <Route path="/login" element={<Login/>}></Route>
-            <Route path="/account" element={<Account/>}></Route>
+            <Route path="/auth" element={<Auth />} />
           </Routes>
         </Router>
       </>
